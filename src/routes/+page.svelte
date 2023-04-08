75456dbbd2694ca6eedaa4fb9fae85f6f386b93f
Ruadhan Keller-Mcleer <9281239@students.k12.dc.us>
2023-04-07 22:44:05 -0400

    auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+    import { initializeApp } from "firebase/app";
+
+    const firebaseConfig = {
+        apiKey: import.meta.env.VITE_APIKEY,
+        authDomain: "bitcamp-2023.firebaseapp.com",
+        projectId: "bitcamp-2023",
+        storageBucket: "bitcamp-2023.appspot.com",
+        messagingSenderId: "674372391347",
+        appId: "1:674372391347:web:30a95f426d836031a2fbc6"
+    };
+
+    const app = initializeApp(firebaseConfig);
+
+    let email : string;
+    let password : string;
+
+    function addUser() {
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                
+                console.log(errorCode, errorMessage)
+            });
+    }
+</script>
+
+<input type="email" bind:value={email}/>
+<input type="password" bind:value={password}/>
+<button on:click={addUser}>Submit</button>
